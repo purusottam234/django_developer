328e606382f54ce0f9f51a27467e8131b159fa01
purusottam234 <purusottamadhikari234@gmail.com>
2022-02-01 14:02:27 +0545

    creating model

--- student/models.py
+++ student/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    Name = models.CharField(max_length=200)
+    Age = models.IntegerField(max_length=10)
+    Address = models.TextField()
+    Grade = models.CharField(max_length=10)
+    Major = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
